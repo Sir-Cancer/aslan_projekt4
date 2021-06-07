99805157db6d3da5ac93ec4458a580c641d298bc
Serkan Aslan Celebi <serkan.a.celebi@gmail.com>
2021-06-07 22:02:17 +0200

    attempted restructuring elements.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth - 60;
-canvas.height = 400;
+canvas.height = 500;
 
 let context = canvas.getContext("2d");
 let start_background_color = "white";
